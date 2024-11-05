51c00109d1bd99e9da9732234d932c72d654d89a
JP Codecraft <jpcode@craft.com>
2024-11-05 14:31:48 -0300

    videos

---
+++ videos/data.js
@@ -0,0 +1,8 @@
+[
+  "https://www.youtube.com/watch?v=JSzGaDH8eaU&ab_channel=YoianAlbion",
+  "https://www.youtube.com/watch?v=wc4GrRVsWcs&t=95s&ab_channel=YoianAlbion",
+  "https://www.youtube.com/watch?v=CdYctYfpP-o&ab_channel=JackVoids",
+  "https://www.youtube.com/watch?v=jw4Gz5MDVs0&t=125s&ab_channel=AlbionFreeMarket",
+  "https://www.youtube.com/watch?v=BcD_gosFTOg&ab_channel=AlbionFreeMarket",
+  "https://www.youtube.com/watch?v=aQ4pNCKHZ5Y&t=3s&ab_channel=AlbionFreeMarket",
+];
